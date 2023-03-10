bc89cf23608e07f77e16b1a50af44211a3299ee6
Samuel Bosawer <zam.bosawer@gmail.com>
2023-03-10 10:48:40 +0900

    Coding PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	// Variabel String
+	$judul = "Hello Word !!";
+	$bilangan = 10; 
+	$nama = "Samuel";
+?>
+<html>
+	<head>
+		<title> PHP Day 00 </title>
+	</head>
+	<body>
+		<h1> PHP Day 00 - FILE PHP</h1>
+		<h2> <?php echo $judul; ?> </h2>
+		<h2> <?= $bilangan ?> </h2>
+		<h2> <?= "Hello ". $nama?> </h2>
+	<body>
+</html>
